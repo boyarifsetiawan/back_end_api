9797878c7aeb09a5a995aeeed44357f7a32927e8
Boy Arief <boysetiawan5635@gmail.com>
2025-11-29 20:40:08 +0700

    add cors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
-
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,11 +26,11 @@ class AppServiceProvider extends ServiceProvider
         );
     }
 
-
-    public function boot()
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
     {
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
+        //
     }
 }
