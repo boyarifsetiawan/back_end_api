c27656a2215618adf7eecd9671f4d908e7686d8a
Boy Arief <boysetiawan5635@gmail.com>
2025-11-30 08:16:32 +0700

    fix api

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Ecommerce\CartController;
 use App\Http\Controllers\Ecommerce\OrderController;
 use App\Http\Controllers\Ecommerce\ProductController;
 use App\Http\Controllers\Ecommerce\CategoryController;
+use App\Http\Controllers\Api\FileManager\FileController;
 use App\Http\Controllers\Api\Portofolio\SkillController;
 use App\Http\Controllers\Api\Portofolio\ProfileController;
 use App\Http\Controllers\Api\Portofolio\ProjectController;
@@ -101,34 +102,34 @@ Route::prefix('multipurpose')->controller(UserController::class)->group(function
 
 
 // FIle manageer
-// Route::controller(\App\Http\Controllers\FileController::class)
-//     ->middleware(['auth', 'verified'])
-//     ->group(function () {
-//         Route::get('/my-files/{folder?}', 'myFiles')
-//             ->where('folder', '(.*)')
-//             ->name('myFiles');
-//         Route::post('/folder/create', 'createFolder')->name('folder.create');
-//         Route::post('/file', 'store')->name('file.store');
-//         Route::delete('/file', 'destroy')->name('file.delete');
+Route::controller(FileController::class)
+    ->middleware(['auth', 'verified'])
+    ->group(function () {
+        Route::get('/my-files/{folder?}', 'myFiles')
+            ->where('folder', '(.*)')
+            ->name('myFiles');
+        Route::post('/folder/create', 'createFolder')->name('folder.create');
+        Route::post('/file', 'store')->name('file.store');
+        Route::delete('/file', 'destroy')->name('file.delete');
 
-//         Route::get('/file/download', 'download')->name('file.download');
-//         Route::get('/trash', 'trash')->name('trash');
-//         Route::post('/file/restore', 'restore')->name('file.restore');
-//         Route::delete('/file/delete-forever', 'deleteForever')->name('file.deleteForever');
-//         Route::post('/file/add-to-favourites', 'addToFavourites')->name('file.addToFavourites');
-//         Route::post('/file/share', 'share')->name('file.share');
-//         Route::get('/shared-with-me', 'sharedWithMe')->name('file.sharedWithMe');
-//         Route::get('/shared-by-me', 'sharedByMe')->name('file.sharedByMe');
-//         Route::get('/file/download-shared-with-me', 'downloadSharedWithMe')->name('file.downloadSharedWithMe');
-//         Route::get('/file/download-shared-by-me', 'downloadSharedByMe')->name('file.downloadSharedByMe');
-//     });
+        Route::get('/file/download', 'download')->name('file.download');
+        Route::get('/trash', 'trash')->name('trash');
+        Route::post('/file/restore', 'restore')->name('file.restore');
+        Route::delete('/file/delete-forever', 'deleteForever')->name('file.deleteForever');
+        Route::post('/file/add-to-favourites', 'addToFavourites')->name('file.addToFavourites');
+        Route::post('/file/share', 'share')->name('file.share');
+        Route::get('/shared-with-me', 'sharedWithMe')->name('file.sharedWithMe');
+        Route::get('/shared-by-me', 'sharedByMe')->name('file.sharedByMe');
+        Route::get('/file/download-shared-with-me', 'downloadSharedWithMe')->name('file.downloadSharedWithMe');
+        Route::get('/file/download-shared-by-me', 'downloadSharedByMe')->name('file.downloadSharedByMe');
+    });
 
 // Route::get('/dashboard', function () {
 //     return Inertia::render('Dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
